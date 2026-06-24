a18b368d153157156e253736a4c0029980eee885
giovana-rosa <giovana.rosaifc@gmail.com>
2026-06-24 13:58:27 -0300

    feat: inclusão da foto de perfil no usuário

--- core/models/user.py
+++ core/models/user.py
@@ -36,12 +36,21 @@ class UserManager(BaseUserManager):
 
         return user
 
+from uploader.models import Image
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User model in the system."""
 
     email = models.EmailField(max_length=255, unique=True, verbose_name=_('email'), help_text=_('Email'))
     name = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('name'), help_text=_('Username'))
+    foto = models.ForeignKey(
+        Image,
+        related_name='user_foto',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
     is_active = models.BooleanField(
         default=True, verbose_name=_('Usuário está ativo'), help_text=_('Indica que este usuário está ativo.')
     )
