514019ca6d79561d201870ad198a0c2d8f7759f8
giovana-rosa <giovana.rosaifc@gmail.com>
2026-08-26 13:58:59 -0300

    refactor: change telefone field from PhoneNumberField to CharField with regex validation

--- core/models/funcionario.py
+++ core/models/funcionario.py
@@ -2,12 +2,21 @@ from django.db import models
 
 from uploader.models import Image
 from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import RegexValidator
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
     cargo = models.CharField(max_length=100)
-    telefone = PhoneNumberField(region='BR', blank=False)
+    telefone = models.CharField(
+        max_length=20,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$',
+                message="Use o formato: '+5511999999999'."
+            )
+        ]
+    )
     foto = models.ForeignKey(
         Image,
         related_name='+',
