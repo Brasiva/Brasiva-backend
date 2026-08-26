7b414fc6d3f46ab9d1fbbe1890c02272d50e8d37
giovana-rosa <giovana.rosaifc@gmail.com>
2026-08-25 21:42:51 -0300

    refactor: campo pagamento removido da model Funcionário e adicionado na model EquipeEvento

--- core/models/equipe_evento.py
+++ core/models/equipe_evento.py
@@ -4,6 +4,7 @@ from django.db import models
 class EquipeEvento(models.Model):
     funcionario = models.ForeignKey('Funcionario', on_delete=models.CASCADE, related_name='equipes_vinculadas')
     evento = models.ForeignKey('Evento', on_delete=models.CASCADE, related_name='funcionarios_vinculados')
+    pagamento = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Equipe do Evento'
