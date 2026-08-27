6b82957dae22d80c9f6177474f6f743f6d23ee76
giovana-rosa <giovana.rosaifc@gmail.com>
2026-08-27 11:23:36 -0300

    refactor: revert telefone field in Funcionario model to PhoneNumberField

--- core/models/funcionario.py
+++ core/models/funcionario.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 from uploader.models import Image
-from django.core.validators import RegexValidator
-# from phonenumber_field.modelfields import PhoneNumberField
+#from django.core.validators import RegexValidator
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
     cargo = models.CharField(max_length=100)
-    # telefone = PhoneNumberField(region='BR', blank=False)
-    telefone = models.CharField(
-        max_length=20, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Use o formato: '+5511999999999'.")]
-    )
+    telefone = PhoneNumberField(region='BR', blank=False)
+    #telefone = models.CharField(
+    #    max_length=20, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Use o formato: '+5511999999999'.")]
+    #)
     foto = models.ForeignKey(
         Image,
         related_name='+',
